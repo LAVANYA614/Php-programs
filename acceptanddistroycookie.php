9360f96d5085a15b828370950711fa2bbb432dee
Lavanya Bhandari <LAV.BHANDARI444@GMAIL.COM>
2026-04-28 21:21:49 +0530

    Create acceptanddistroycookie.php

---
+++ acceptanddistroycookie.php
@@ -0,0 +1,6 @@
+<!-- accept the cookie wehre variable is erp and erp id  and expiry is 1 min -->
+<?php
+setcookie("erp", "12345", time() + 60);
+echo "Cookie 'erp' has been set with value '12345' and will expire in 1 minute.";
+echo "This code is written and executed by Lavanya Bhandari with erp-0231BCA036";
+?>
