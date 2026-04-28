a3293133127ec917a9b4ecefff3129827f6e7343
Lavanya Bhandari <LAV.BHANDARI444@GMAIL.COM>
2026-04-28 21:30:32 +0530

    Create array-func-asort.php

---
+++ array-func-asort.php
@@ -0,0 +1,7 @@
+<?php
+#asort function
+$age = array("Peter"=>"20", "Harry"=>"14", "John"=>"45", "Clark"=>"35");
+asort($age);
+print_r($age);
+echo "This code is written and executed by Lavanya Bhandari with erp-0231BCA036";
+?>
